e3550bb70f9a6d3b2cd8f3ff1afd2e402b9aad37
guilherme-wolf <63266766+guilherme-wolf@users.noreply.github.com>
2020-05-21 12:55:25 -0300

    Desafio 1 - 1 RocketSeat

---
+++ Desafio01.js
@@ -0,0 +1,80 @@
+/* 1 crie um algoritmo para tomar banho 
+
+1 - Vá até o banheiro 
+2 - Acenda as luzes 
+3 - feche a porta
+4 - tire a roupa
+5 - entre no box
+6 - Ligue o chuveiro 
+7- Tome o banho
+
+*/
+
+/* 2 Faça um algoritmo para somar dois numeros e multiplicar o resultado pelo primeiro numnero */
+
+/*const numero1 = 8;
+const numero2 = 9;
+
+const sum = numero1 + numero2;
+const result = sum * numero1;
+
+console.log (result) */
+
+/*3 Descreva com detalhes a sequência lógica para trocar um pneu de um carro
+
+
+    1 - Estacione o carro
+    2 - ligue o pisca alerta
+    3 - desligue o carro
+    4 - tire o cinto de segurança
+    5 - abra a porta 
+    6 - desça do carro
+    7 - fecha a porta do carro
+    8 - caminhe até o porta malas
+    9 - abra o porta malas
+    10 - retire o step do porta malas
+    11 - retire o pneu do porta malas
+    12 - coloque o macaco de baixo do carro
+    13 - vai subir o carro 1 metro do chão 
+    14 - pegue a chave de roda
+    15 - retire os parafuços da roda
+    16 - retire a roda
+    17 - coloque no lugar a nova roda
+    18 - rosqueie a roda 
+    19 - teste para ver se está firme
+    20 - desça o carro ultilizando o macaco
+    21 - retire o macaco debaixo do carro
+    22 - guarde o macaco no porta malas
+    23 - guarde o step no porta malas
+    24 - guarde o pneu trocado no porta malas
+    25 - feche o porta malas
+    26 - abra a porta do carro
+    27 - entre no carro
+    28 - feche a porta do carro 
+    29 - coloque o cinto
+    30 - ligue o carro 
+    31 - va embora
+
+
+
+
+
+*/
+
+
+
+const diasDaSemana = ["segunda", "terça", "quarta", "quinta", "sexta"];
+const comer = "Bolinho"; 
+
+
+let i = 0; 
+    for (i = 0; i >= 0; diasDaSemana.length++){
+        console.log (`Hoje é dia de ${comer}!!`)
+    }
+
+console.log (diasDaSemana)
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,14 @@
+const nome = 'Guilherme';
+const peso = 65;
+const altura = 1.50;
+
+
+const imc = peso / (altura * altura);
+
+if (imc >= 30 ){
+    console.log (`${nome} você está acima do peso!!!`)
+}if (imc < 29.9){
+    console.log (`${nome} você não está acima do peso !!!`)
+}
+
+console.log (imc)
