b9df37bf5cf097acb2387ba7c01221c1cf268e35
guilherme-wolf <63266766+guilherme-wolf@users.noreply.github.com>
2020-05-30 15:16:44 -0300

    att dos codigos

---
+++ aposentadoria.js
@@ -0,0 +1,20 @@
+const nome = "Silvana";
+const sexo = "F";
+const idade = 56;
+const contribuicao = 30;
+
+const TempoMinimoDeContribuicaoHomem = 35
+const TempoMinimoDeContribuicaoMulher = 30
+
+
+const soma = idade + contribuicao 
+
+
+if (contribuicao == TempoMinimoDeContribuicaoMulher){  
+}
+if (soma >= 85 ){
+    console.log (`${nome} Você pode se aposentar!`)
+}else{
+    console.log (`${nome} Você NÃO pode se aposentar!`)
+
+}
